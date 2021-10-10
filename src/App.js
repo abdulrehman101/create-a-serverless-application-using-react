8efa2fb4e8c23e29fb0120c5ded53d972874e451
abdulrehman101 <abdulr101@outlook.com>
2021-10-10 00:50:41 -0500

    addedd json to represent dynamic data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is a perfect marriage of javascript and markup
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Header/>
+
+        <Main />
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
